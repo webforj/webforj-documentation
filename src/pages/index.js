43ee1bcf459c8ea567b3840d0a1586b52af592b3
Matthew Hawkins <mhawkins@basis.cloud>
2023-02-17 16:56:37 +0100

    Temporary solution for style issues when updated to 2.3.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,9 +38,10 @@ function HomepageHeader() {
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout className='homepage'
+    <Layout
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
+        <div className='homepage'>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
@@ -48,6 +49,7 @@ export default function Home() {
         test
       </div> */}
       </main>
+        </div>
     </Layout>
   );
 }
