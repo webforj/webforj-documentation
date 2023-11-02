24a8b8efdcdf4cf103cc02b77899aa404e973582
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-11-02 17:19:06 +0100

    Updated the TableBuilder to properly show the reflected attributes

--- src/components/DocsTools/TableBuilder.js
+++ src/components/DocsTools/TableBuilder.js
@@ -132,7 +132,7 @@ export default function TableBuilder(props) {
         </thead>
         <tbody>
           {reflectItems?.map((prop) => (
-            prop.attr != null && prop.display == false && !props.exclusions?.includes(prop.attr) && (
+            prop.attr != null && prop.display == true && !props.exclusions?.includes(prop.attr) && (
             <tr key={prop.attr}>
               <td><code>{prop.attr}</code></td>
               <td>{prop.desc}</td>
