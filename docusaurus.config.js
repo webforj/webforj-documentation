449f9da965f256abad4f42a00e8d6f151df2e505
MatthewHawkins <matthew.hawkins1115@gmail.com>
2024-07-24 16:14:52 +0200

    chore: minor updates to blog front matter, css changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,11 +90,11 @@ const config = {
         //... other Algolia params
       },
       announcementBar: {
-        id: 'version_release',
+        id: '24.10',
         content:
           'We are excited to announce webforJ version 24.10 is live! Read more about the changes and features <a href=/blog/whats-new-24.10>here.</a> ',
-        backgroundColor: '#303846',
-        textColor: '#ebedf0',
+        // backgroundColor: 'var(--announcement-background)',
+        // textColor: '#ebedf0',
         // backgroundColor: 'var(--chip-background)',
         // textColor: 'var(--chip-text)',
         isCloseable: true,
