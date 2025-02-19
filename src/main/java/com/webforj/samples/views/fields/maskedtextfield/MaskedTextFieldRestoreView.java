dbec0d7c62684919da7ffd79be0334caae0828a8
Eric Handtke <ehandtke@basis.cloud>
2025-02-19 14:15:29 +0100

    Added samples

---
+++ src/main/java/com/webforj/samples/views/fields/maskedtextfield/MaskedTextFieldRestoreView.java
@@ -0,0 +1,32 @@
+package com.webforj.samples.views.fields.maskedtextfield;
+
+import com.webforj.component.Composite;
+import com.webforj.component.button.Button;
+import com.webforj.component.field.MaskedTextField;
+import com.webforj.component.layout.flexlayout.FlexAlignment;
+import com.webforj.component.layout.flexlayout.FlexDirection;
+import com.webforj.component.layout.flexlayout.FlexJustifyContent;
+import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.router.annotation.Route;
+
+@Route
+public class MaskedTextFieldRestoreView extends Composite<FlexLayout>{
+    MaskedTextField usernameField = new MaskedTextField("Username", "WebforJUser");
+
+    public MaskedTextFieldRestoreView() {
+        FlexLayout self = getBoundComponent();
+        usernameField.setRestoreValue("WebforJUser");
+        self.setDirection(FlexDirection.COLUMN)
+                .setJustifyContent(FlexJustifyContent.CENTER)
+                .setAlignment(FlexAlignment.CENTER)
+                .setSpacing("var(--dwc-space-m)")
+                .setMargin("var(--dwc-space-m)");
+
+                Button restoreButton = new Button("Reset Username", event -> {
+                    usernameField.restoreValue();
+                });
+        
+        self.add(usernameField, restoreButton);
+    }
+}
+
