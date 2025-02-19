cf3b8a9147afe65c294a0186cf8e0ab0c2782be4
Eric Handtke <ehandtke@basis.cloud>
2025-02-19 14:15:29 +0100

    Added samples

---
+++ src/main/java/com/webforj/samples/views/fields/maskedtextfield/MaskedTextFieldSpinnerView.java
@@ -0,0 +1,35 @@
+package com.webforj.samples.views.fields.maskedtextfield;
+
+import java.util.List;
+
+import com.webforj.component.Composite;
+import com.webforj.component.field.MaskedTextFieldSpinner;
+import com.webforj.component.layout.flexlayout.FlexAlignment;
+import com.webforj.component.layout.flexlayout.FlexDirection;
+import com.webforj.component.layout.flexlayout.FlexJustifyContent;
+import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.router.annotation.Route;
+
+@Route
+public class MaskedTextFieldSpinnerView extends Composite<FlexLayout>{
+
+    MaskedTextFieldSpinner spinner = new MaskedTextFieldSpinner();
+
+    public MaskedTextFieldSpinnerView() {
+        FlexLayout self = getBoundComponent();
+        self.setDirection(FlexDirection.COLUMN)
+                .setJustifyContent(FlexJustifyContent.CENTER)
+                .setAlignment(FlexAlignment.CENTER)
+                .setSpacing("var(--dwc-space-m)")
+                .setMargin("var(--dwc-space-m)");
+
+                spinner.setPattern("[A-Za-z]*");
+                spinner.setTooltipText("Current status:");
+                spinner.setOptions(List.of("Active","Busy", "Away"));
+
+                spinner.setValue("Monday");
+  
+    
+        self.add(spinner);
+    }
+}
