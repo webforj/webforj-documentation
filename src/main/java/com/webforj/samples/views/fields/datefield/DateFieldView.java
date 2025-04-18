35bed79ea584f9f5e3e0e8404c7d6204e74167d2
Lauren Alamo <laurenalamo@outlook.com>
2025-04-17 20:01:04 -0400

    added valuechangelistener to ensure valid return date
    - Renamed variables for clarity (departure → departureField, arrivalDate
    → returnDate)

--- src/main/java/com/webforj/samples/views/fields/datefield/DateFieldView.java
+++ src/main/java/com/webforj/samples/views/fields/datefield/DateFieldView.java
@@ -1,6 +1,7 @@
 package com.webforj.samples.views.fields.datefield;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import com.webforj.component.Composite;
 import com.webforj.component.field.DateField;
@@ -14,38 +15,45 @@ import com.webforj.router.annotation.Route;
 @FrameTitle("Date Field Demo")
 public class DateFieldView extends Composite<FlexLayout> {
 
+  DateField departureField = new DateField(LocalDate.now());
   DateField returnField = new DateField(LocalDate.now());
-  DateField departure = new DateField(LocalDate.now());
 
   public DateFieldView() {
-    getBoundComponent()
-        .setDirection(FlexDirection.ROW)
-        .setSpacing("var(--dwc-space-l)")
-        .setMargin("var(--dwc-space-m)");
+    getBoundComponent().setDirection(FlexDirection.ROW)
+      .setSpacing("var(--dwc-space-l)")
+      .setMargin("var(--dwc-space-m)");
 
-    departure.setLabel("Departure Date:")
-        .setWidth("200px")
-        .addValueChangeListener(this::setMinReturn);
+    departureField.setLabel("Departure Date:")
+      .setWidth("200px")
+      .setMin(LocalDate.now())
+      .addValueChangeListener(this::setMinReturn);
 
     returnField.setLabel("Return Date:")
-        .setWidth("200px")
-        .setMin(LocalDate.now());
+      .setWidth("200px")
+      .setMin(LocalDate.now())
+      .addValueChangeListener(this::validateReturnField);
 
-    getBoundComponent().add(departure, returnField);
+    getBoundComponent().add(departureField, returnField);
   }
 
   private void setMinReturn(ValueChangeEvent e) {
-    Object val = e.getValue();
-    if (!(val instanceof LocalDate departureDate)) {
-      return;
+    try {
+      LocalDate departureDate = (LocalDate) e.getValue();
+      LocalDate returnDate = returnField.getValue();
+
+      if (departureDate != null && returnDate != null && departureDate.isAfter(returnDate)) {
+        returnField.setValue(departureDate);
+      }
+
+      returnField.setMin(departureDate);
+    } catch (DateTimeParseException | NullPointerException ex) {
     }
+  }
 
-    LocalDate arrivalDate = returnField.getValue();
-
-    if (departureDate.isAfter(arrivalDate)) {
-      returnField.setValue(departureDate);
+  private void validateReturnField(ValueChangeEvent e) {
+    try {
+      LocalDate val = (LocalDate) e.getValue();
+    } catch (DateTimeParseException | NullPointerException ex) {
     }
-
-    returnField.setMin(departureDate);
   }
 }
