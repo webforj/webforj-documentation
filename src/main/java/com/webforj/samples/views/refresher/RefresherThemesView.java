d5a43dbfed066189b570b41766b8613a1949b10c
Lauren Alamo <laurenalamo@outlook.com>
2025-05-07 22:12:22 -0400

    doc: new refresher docs and demos

---
+++ src/main/java/com/webforj/samples/views/refresher/RefresherThemesView.java
@@ -0,0 +1,83 @@
+package com.webforj.samples.views.refresher;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.webforj.annotation.InlineStyleSheet;
+import com.webforj.component.Composite;
+import com.webforj.component.Theme;
+import com.webforj.component.html.elements.Div;
+import com.webforj.component.icons.FeatherIcon;
+import com.webforj.component.icons.Icon;
+import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.component.refresher.Refresher;
+import com.webforj.router.annotation.Route;
+
+@Route
+@InlineStyleSheet("context://css/refresher/refresher.css")
+public class RefresherThemesView extends Composite<Div> {
+
+  private final List<Theme> themes = List.of(
+    Theme.PRIMARY, Theme.SUCCESS, Theme.WARNING,
+    Theme.DANGER, Theme.INFO, Theme.GRAY, Theme.DANGER
+  );
+
+  private final AtomicInteger themeIndex = new AtomicInteger(0);
+  private final Random random = new Random();
+  private final String[] names = {
+    "John", "Jane", "Alice", "Bob", "Charlie", "Diana",
+    "Ethan", "Fiona", "George", "Hannah", "Ian", "Jill"
+  };
+
+  public RefresherThemesView() {
+    Div self = getBoundComponent();
+    self.addClassName("theme-container");
+
+    Div canvas = new Div().addClassName("is-canvas");
+    self.add(canvas);
+
+    Refresher refresher = new Refresher()
+        .setTheme(themes.get(0));
+
+    refresher.onRefresh(e -> {
+      canvas.removeAll();
+      for (int i = 0; i < 6; i++) {
+        canvas.add(new Item());
+      }
+
+      int next = themeIndex.updateAndGet(i -> (i + 1) % themes.size());
+      Theme nextTheme = themes.get(next);
+      refresher.setTheme(nextTheme);
+      refresher.finish();
+    });
+
+    self.add(refresher);
+
+    for (int i = 0; i < 6; i++) {
+      canvas.add(new Item());
+    }
+  }
+
+  class Item extends Composite<Div> {
+    public Item() {
+      Div self = getBoundComponent();
+
+      String name = names[random.nextInt(names.length)];
+
+      Div nameDiv = new Div(name).addClassName("item-name");
+      Div excerpt = new Div("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+          + "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+          .addClassName("item-excerpt");
+
+      Icon icon = FeatherIcon.ARROW_RIGHT.create();
+      icon.setMinSize("24px", "24px");
+
+      self.add(FlexLayout.create(FlexLayout.create(nameDiv, excerpt).vertical().build(), icon)
+          .horizontal()
+          .justify().between()
+          .align().center()
+          .build());
+    }
+  }
+}
