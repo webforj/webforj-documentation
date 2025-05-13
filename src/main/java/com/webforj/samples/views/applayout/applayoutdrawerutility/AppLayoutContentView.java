cd8cc2cdc5b39bcb92e16c5d55d2a5b3a0a6c4a3
Eric Handtke <ehandtke@basis.cloud>
2025-05-13 11:28:53 +0200

    added drawer utility demo.

---
+++ src/main/java/com/webforj/samples/views/applayout/applayoutdrawerutility/AppLayoutContentView.java
@@ -0,0 +1,9 @@
+package com.webforj.samples.views.applayout.applayoutdrawerutility;
+
+import com.webforj.router.annotation.Route;
+import com.webforj.samples.views.applayout.AbstractContentView;
+
+@Route(value = "/content/:name", outlet = AppLayoutDrawerUtilityView.class)
+public class AppLayoutContentView extends AbstractContentView{
+
+}
