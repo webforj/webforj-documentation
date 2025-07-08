f8adcd3db6dd6b8ca81285180d3ccb735d809532
Hyyan Abo Fakher <hyyanaf@gmail.com>
2025-07-08 13:37:38 +0200

    Update GiscusComments component configuration and import in MDXComponents

--- docs/src/components/DocsTools/GiscusComments.js
+++ docs/src/components/DocsTools/GiscusComments.js
@@ -27,12 +27,12 @@ export default function GiscusComments() {
     if (ref.current && !ref.current.hasChildNodes()) {
       const script = document.createElement('script');
       script.src = 'https://giscus.app/client.js';
-      script.setAttribute('data-repo', 'webforj/blog-comments');
-      script.setAttribute('data-repo-id', 'R_kgDOPITvyA');
-      script.setAttribute('data-category', 'Announcements');
-      script.setAttribute('data-category-id', 'DIC_kwDOPITvyM4Csl11');
+      script.setAttribute('data-repo', 'webforj/webforj-documentation');
+      script.setAttribute('data-repo-id', 'R_kgDONxZPwg');
+      script.setAttribute('data-category', 'Comments');
+      script.setAttribute('data-category-id', 'DIC_kwDONxZPws4CspSY');
       script.setAttribute('data-mapping', 'title');
-      script.setAttribute('data-strict', '0');
+      script.setAttribute('data-strict', '1');
       script.setAttribute('data-reactions-enabled', '1');
       script.setAttribute('data-emit-metadata', '0');
       script.setAttribute('data-input-position', 'bottom');
--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -12,6 +12,7 @@ import ParentLink from '@site/src/components/DocsTools/ParentLink';
 import TableBuilder from '@site/src/components/DocsTools/TableBuilder';
 import TabSwitcher from '@site/src/components/DocsTools/TabSwitcher';
 import ComponentArchetype from '@site/src/components/DocsTools/ComponentArchetype';
+import GiscusComments from '@site/src/components/DocsTools/GiscusComments';
 import DocCardList from '@theme/DocCardList';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
@@ -33,5 +34,6 @@ export default {
   ComponentArchetype,
   Tabs,
   TabItem,
+  GiscusComments
 };
 
