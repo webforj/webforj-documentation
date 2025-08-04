ed2f484c29b91eb965010fcea8e1bda233d1f677
MatthewHawkins <matthew.hawkins1115@gmail.com>
2025-08-04 14:22:24 -0400

    chore: starting fresh for more testing

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,37 +14,37 @@ const config = {
   organizationName: 'webforj',
   projectName: 'webforj-docs',
   trailingSlash: false,
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es', 'fr', 'de', 'nl'],
-    localeConfigs: {
-      en: {
-        label: 'English',
-        direction: 'ltr',
-        htmlLang: 'en-US',
-      },
-      es: {
-        label: 'Español',
-        direction: 'ltr',
-        htmlLang: 'es-ES',
-      },
-      fr: {
-        label: 'Français',
-        direction: 'ltr',
-        htmlLang: 'fr-FR',
-      },
-      de: {
-        label: 'Deutsch',
-        direction: 'ltr',
-        htmlLang: 'de-DE',
-      },
-      nl: {
-        label: 'Nederlands',
-        direction: 'ltr',
-        htmlLang: 'nl-NL',
-      },
-    },
-  },
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['en', 'es', 'de'],
+  //   localeConfigs: {
+  //     en: {
+  //       label: 'English',
+  //       direction: 'ltr',
+  //       htmlLang: 'en-US',
+  //     },
+  //     es: {
+  //       label: 'Español',
+  //       direction: 'ltr',
+  //       htmlLang: 'es-ES',
+  //     },
+  //     fr: {
+  //       label: 'Français',
+  //       direction: 'ltr',
+  //       htmlLang: 'fr-FR',
+  //     },
+  //     de: {
+  //       label: 'Deutsch',
+  //       direction: 'ltr',
+  //       htmlLang: 'de-DE',
+  //     },
+  //     nl: {
+  //       label: 'Nederlands',
+  //       direction: 'ltr',
+  //       htmlLang: 'nl-NL',
+  //     },
+  //   },
+  // },
   scripts: [
     { src: '/js/dwc-theme-switcher.js', async: false },
     { src: '/js/link-decorator.js' }
