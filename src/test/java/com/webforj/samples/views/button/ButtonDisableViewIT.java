cea1a05a86d7112b2bf017d466c2ca55bea7ddd9
cliebhardt <liebhardt.christian@proton.me>
2026-01-27 22:29:25 +0100

    test: Updated Button integration tests

--- src/test/java/com/webforj/samples/views/button/ButtonDisableViewIT.java
+++ src/test/java/com/webforj/samples/views/button/ButtonDisableViewIT.java
@@ -2,32 +2,39 @@ package com.webforj.samples.views.button;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
+import com.webforj.samples.pages.SupportedLanguage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.webforj.samples.pages.button.ButtonDisablePage;
 import com.webforj.samples.views.BaseTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ButtonDisableViewIT extends BaseTest {
 
-    private ButtonDisablePage buttonDisable;
+  private ButtonDisablePage buttonDisable;
 
-    @BeforeEach
-    public void setupButtonDisableDemo() {
-        navigateToRoute(ButtonDisablePage.getRoute());
-        buttonDisable = new ButtonDisablePage(page);
-    }
+  public void setupButtonDisableDemo(SupportedLanguage language) {
+    navigateToRoute(ButtonDisablePage.getRoute(language));
+    buttonDisable = new ButtonDisablePage(page);
+  }
 
-    @Test
-    public void testSubmitButtonIsDisabledWhenEmailInputIsNotValid() {
-        buttonDisable.getEmailInput().fill("invalid-email");
-        assertThat(buttonDisable.getSubmitButton()).isDisabled();
-    }
+  @ParameterizedTest
+  @MethodSource("provideRoutes")
+  public void testSubmitButtonIsDisabledWhenEmailInputIsNotValid(SupportedLanguage language) {
+    setupButtonDisableDemo(language);
+    buttonDisable.getEmailInput().fill("invalid-email");
+    assertThat(buttonDisable.getSubmitButton()).isDisabled();
+  }
 
-    @Test
-    public void testSubmitButtonIsEnabledWhenEmailInputIsValid() {
-        buttonDisable.getEmailInput().fill("valid-email@example.com");
-        assertThat(buttonDisable.getSubmitButton()).isEnabled();
-    }
+
+  @ParameterizedTest
+  @MethodSource("provideRoutes")
+  public void testSubmitButtonIsEnabledWhenEmailInputIsValid(SupportedLanguage language) {
+    setupButtonDisableDemo(language);
+    buttonDisable.getEmailInput().fill("valid-email@example.com");
+    assertThat(buttonDisable.getSubmitButton()).isEnabled();
+  }
 
 }
