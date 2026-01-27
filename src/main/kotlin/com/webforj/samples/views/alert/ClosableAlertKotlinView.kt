218043f14ed75e179f91644ba6bf12fc3ecc0dc2
cliebhardt <liebhardt.christian@gmail.com>
2026-01-27 10:37:04 +0100

    feat: ClosableAlertKotlinView

---
+++ src/main/kotlin/com/webforj/samples/views/alert/ClosableAlertKotlinView.kt
@@ -0,0 +1,44 @@
+package com.webforj.samples.views.alert
+
+import com.basis.bbj.comm.al
+import com.webforj.component.Composite
+import com.webforj.component.Theme
+import com.webforj.component.alert.Alert
+import com.webforj.component.button.Button
+import com.webforj.component.button.ButtonTheme
+import com.webforj.component.layout.flexlayout.FlexAlignment
+import com.webforj.component.layout.flexlayout.FlexDirection
+import com.webforj.component.layout.flexlayout.FlexJustifyContent
+import com.webforj.component.layout.flexlayout.FlexLayout
+import com.webforj.kotlin.dsl.component.alert.alert
+import com.webforj.kotlin.dsl.component.button.button
+import com.webforj.router.annotation.FrameTitle
+import com.webforj.router.annotation.Route
+
+@Route
+@FrameTitle("Closable Alert Kotlin")
+class ClosableAlertKotlinView: Composite<FlexLayout>() {
+  private val reopenButton: Button
+  private val alert: Alert
+
+  init {
+    boundComponent.apply {
+      direction = FlexDirection.COLUMN
+      spacing = "var(--dwc-space-m)"
+      alignment = FlexAlignment.CENTER
+      justifyContent = FlexJustifyContent.CENTER
+      margin = "var(--dwc-space-l)"
+      alert = alert("Heads up! This alert can be dismissed.", Theme.INFO, true) {
+        maxWidth = "400px"
+        onClose { reopenButton.isVisible = true }
+      }
+      reopenButton = button("Show Alert", ButtonTheme.PRIMARY) {
+        isVisible = false
+        onClick {
+          alert.open()
+          isVisible = false
+        }
+      }
+    }
+  }
+}
