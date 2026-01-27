eb980097e6bf84fe988a1c5aba7b3b28cb756b6d
cliebhardt <liebhardt.christian@proton.me>
2026-01-27 23:26:42 +0100

    feat: Added CheckboxExpanseKotlinView

---
+++ src/main/kotlin/com/webforj/samples/views/checkbox/CheckboxExpanseKotlinView.kt
@@ -0,0 +1,30 @@
+package com.webforj.samples.views.checkbox
+
+import com.webforj.component.Composite
+import com.webforj.component.Expanse
+import com.webforj.component.layout.flexlayout.FlexJustifyContent
+import com.webforj.component.layout.flexlayout.FlexLayout
+import com.webforj.component.layout.flexlayout.FlexWrap
+import com.webforj.kotlin.dsl.component.optioninput.checkBox
+import com.webforj.router.annotation.FrameTitle
+import com.webforj.router.annotation.Route
+
+@Route
+@FrameTitle("Checkbox Expanses")
+class CheckboxExpanseKotlinView: Composite<FlexLayout>() {
+
+  init {
+      boundComponent.apply {
+        wrap = FlexWrap.WRAP
+        margin = "var(--dwc-space-l)"
+        spacing = "50px"
+        justifyContent = FlexJustifyContent.CENTER
+        width = "100%"
+        Expanse.entries.reversed().forEach {
+          checkBox(it.name) {
+            expanse = it
+          }
+        }
+      }
+  }
+}
