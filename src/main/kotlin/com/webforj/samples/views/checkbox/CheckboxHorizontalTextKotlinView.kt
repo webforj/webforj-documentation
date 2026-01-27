5b8fb1012c12e8d78ef9645b5793156b6d07d4a9
cliebhardt <liebhardt.christian@proton.me>
2026-01-27 23:32:46 +0100

    feat: Added CheckboxHorizontalTextKotlinView

---
+++ src/main/kotlin/com/webforj/samples/views/checkbox/CheckboxHorizontalTextKotlinView.kt
@@ -0,0 +1,57 @@
+package com.webforj.samples.views.checkbox
+
+import com.webforj.component.Composite
+import com.webforj.component.layout.flexlayout.FlexAlignment
+import com.webforj.component.layout.flexlayout.FlexDirection
+import com.webforj.component.layout.flexlayout.FlexLayout
+import com.webforj.component.layout.flexlayout.FlexWrap
+import com.webforj.concern.HasTextPosition
+import com.webforj.concern.HasTextPosition.Position
+import com.webforj.kotlin.dsl.component.layout.flexlayout.flexLayout
+import com.webforj.kotlin.dsl.component.layout.flexlayout.vertical
+import com.webforj.kotlin.dsl.component.optioninput.checkBox
+import com.webforj.router.annotation.FrameTitle
+import com.webforj.router.annotation.Route
+
+@Route
+@FrameTitle("Checkbox Horizontal Text")
+class CheckboxHorizontalTextKotlinView: Composite<FlexLayout>() {
+
+  init {
+      boundComponent.apply {
+        direction = FlexDirection.ROW
+        wrap = FlexWrap.WRAP
+        spacing = "var(--dwc-space-l)"
+        margin = "var(--dwc-space-l)"
+        flexLayout {
+          vertical()
+          width = "100px"
+          checkBox("Daily", true)
+          checkBox("Weekly")
+          checkBox("Bi-Weekly")
+          checkBox("Monthly")
+          checkBox("Annually")
+        }
+        flexLayout {
+          vertical()
+          alignment = FlexAlignment.END
+          width = "100px"
+          checkBox("Daily", true) {
+            textPosition = Position.LEFT
+          }
+          checkBox("Weekly") {
+            textPosition = Position.LEFT
+          }
+          checkBox("Bi-Weekly") {
+            textPosition = Position.LEFT
+          }
+          checkBox("Monthly") {
+            textPosition = Position.LEFT
+          }
+          checkBox("Annually") {
+            textPosition = Position.LEFT
+          }
+        }
+      }
+  }
+}
