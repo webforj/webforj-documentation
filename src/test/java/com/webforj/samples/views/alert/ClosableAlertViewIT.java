745ee6a613c0360542a0e038a0fa374746c9935f
cliebhardt <liebhardt.christian@gmail.com>
2026-01-27 10:53:25 +0100

    test: Updated Alert test suite
    - Add static getRoute to ClosableAlertPage
    - Added SupportedLanguage argument to getRoute of AlertExpansesPage and
    AlertThemesPage
    - Removed BeforeEach Annotation from setup methods of integration tests
    and added String route argument
    - Parameterized tests

--- src/test/java/com/webforj/samples/views/alert/ClosableAlertViewIT.java
+++ src/test/java/com/webforj/samples/views/alert/ClosableAlertViewIT.java
@@ -2,22 +2,36 @@ package com.webforj.samples.views.alert;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
+import com.webforj.samples.pages.SupportedLanguage;
+import com.webforj.samples.pages.alert.AlertThemesPage;
+import com.webforj.samples.pages.alert.ClosableAlertPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.options.AriaRole;
 import com.webforj.samples.views.BaseTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class ClosableAlertViewIT extends BaseTest {
 
-    @BeforeEach
-    public void setupClosableAlert() {
-        navigateToRoute("closablealert");
+
+    Stream<String> provideRoutes() {
+        return Stream.of(SupportedLanguage.values())
+                .map(ClosableAlertPage::getRoute);
     }
 
-    @Test
-    public void testAlertClosesWhenCloseButtonIsClicked() {
+    public void setupClosableAlert(String route) {
+        navigateToRoute(route);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testAlertClosesWhenCloseButtonIsClicked(String route) {
+        setupClosableAlert(route);
         Locator alert = page.getByRole(AriaRole.ALERT);
 
         assertThat(alert).isVisible();
