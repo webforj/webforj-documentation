c763ffe6af78baab32900f425bc996d45c3385d5
cliebhardt <liebhardt.christian@gmail.com>
2026-01-27 10:53:25 +0100

    test: Updated Alert test suite
    - Add static getRoute to ClosableAlertPage
    - Added SupportedLanguage argument to getRoute of AlertExpansesPage and
    AlertThemesPage
    - Removed BeforeEach Annotation from setup methods of integration tests
    and added String route argument
    - Parameterized tests

--- src/test/java/com/webforj/samples/views/alert/AlertThemesViewIT.java
+++ src/test/java/com/webforj/samples/views/alert/AlertThemesViewIT.java
@@ -1,5 +1,7 @@
 package com.webforj.samples.views.alert;
 
+import com.webforj.samples.pages.SupportedLanguage;
+import com.webforj.samples.pages.alert.AlertExpansesPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,19 +9,29 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 import com.webforj.samples.pages.alert.AlertThemesPage;
 import com.webforj.samples.views.BaseTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class AlertThemesViewIT extends BaseTest {
 
     private AlertThemesPage alertThemesPage;
 
-    @BeforeEach
-    public void setupAlertThemes() {
-        navigateToRoute(AlertThemesPage.getRoute());
+    Stream<String> provideRoutes() {
+        return Stream.of(SupportedLanguage.values())
+                .map(AlertThemesPage::getRoute);
+    }
+
+    public void setupAlertThemes(String route) {
+        navigateToRoute(route);
         alertThemesPage = new AlertThemesPage(page);
     }
 
-    @Test
-    public void testAlertThemes() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testAlertThemes(String route) {
+        setupAlertThemes(route);
         assertThat(alertThemesPage.getSuccessAlert()).hasAttribute("theme", "success");
     }
 
