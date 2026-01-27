294591e00640ea0cdb6640afc188be3006ab3c92
cliebhardt <liebhardt.christian@proton.me>
2026-01-28 00:01:07 +0100

    feat: Added DialogAlignmentsKotlinView

---
+++ src/main/kotlin/com/webforj/samples/views/dialog/DialogAlignmentsKotlinView.kt
@@ -0,0 +1,40 @@
+package com.webforj.samples.views.dialog
+
+import com.webforj.component.Composite
+import com.webforj.component.dialog.Dialog
+import com.webforj.component.layout.flexlayout.FlexLayout
+import com.webforj.kotlin.dsl.component.dialog.dialog
+import com.webforj.kotlin.dsl.component.dialog.header
+import com.webforj.kotlin.dsl.component.html.elements.div
+import com.webforj.kotlin.dsl.component.list.choiceBox
+import com.webforj.kotlin.dsl.component.list.listItem
+import com.webforj.kotlin.extension.set
+import com.webforj.kotlin.extension.styles
+import com.webforj.router.annotation.FrameTitle
+import com.webforj.router.annotation.Route
+
+@Route
+@FrameTitle("Dialog Alignments")
+class DialogAlignmentsKotlinView: Composite<FlexLayout>() {
+
+  init {
+      boundComponent.apply {
+        dialog {
+          styles["display"] = "flex"
+          styles["justify-content"] = "center"
+          setCloseable(false)
+          header { div("Alignments") }
+          choiceBox("Select Alignment") {
+            listItem("Top", Dialog.Alignment.TOP)
+            listItem("Center", Dialog.Alignment.CENTER)
+            listItem("Bottom", Dialog.Alignment.BOTTOM)
+            selectIndex(1)
+            onSelect {
+              this@dialog.alignment = it.selectedItem.key as Dialog.Alignment
+            }
+          }
+          open()
+        }
+      }
+  }
+}
