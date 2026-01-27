5ed0df2aad3d656a9d9b556063fa19e811a863da
cliebhardt <liebhardt.christian@gmail.com>
2026-01-27 11:37:32 +0100

    test: Parameterized AppLayoutViewIT

--- src/test/java/com/webforj/samples/pages/applayout/applayout/AppLayoutPage.java
+++ src/test/java/com/webforj/samples/pages/applayout/applayout/AppLayoutPage.java
@@ -3,6 +3,7 @@ package com.webforj.samples.pages.applayout.applayout;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
+import com.webforj.samples.pages.SupportedLanguage;
 
 public class AppLayoutPage {
 
@@ -15,8 +16,8 @@ public class AppLayoutPage {
 
     }
 
-    public static String getRoute() {
-        return ROUTE;
+    public static String getRoute(SupportedLanguage language) {
+        return language.getPath(ROUTE);
     }
 
     public Locator getDashboardLink() {
--- src/test/java/com/webforj/samples/views/applayout/applayout/AppLayoutViewIT.java
+++ src/test/java/com/webforj/samples/views/applayout/applayout/AppLayoutViewIT.java
@@ -2,24 +2,33 @@ package com.webforj.samples.views.applayout.applayout;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
+import com.webforj.samples.pages.SupportedLanguage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.webforj.samples.pages.applayout.applayout.AppLayoutPage;
 import com.webforj.samples.views.BaseTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class AppLayoutViewIT extends BaseTest{
 
     private AppLayoutPage appLayoutPage;
 
-    @BeforeEach
-    public void setupAppLayout() {
-        navigateToRoute(AppLayoutPage.getRoute());
+    @Override
+    protected String getRoute(SupportedLanguage language) {
+        return AppLayoutPage.getRoute(language);
+    }
+
+    public void setupAppLayout(String route) {
+        navigateToRoute(route);
         appLayoutPage = new AppLayoutPage(page);
     }
 
-    @Test
-    public void testDashboardLink() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testDashboardLink(String route) {
+        setupAppLayout(route);
         appLayoutPage.getDashboardLink().click();
         assertThat(page.getByText("Content for Dashboard")).isVisible();
     }
