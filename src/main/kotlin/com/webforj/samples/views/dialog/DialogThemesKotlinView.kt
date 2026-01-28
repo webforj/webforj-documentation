22636ddd408f0a1f939b7e7c9a8ac340d5c2992b
cliebhardt <liebhardt.christian@proton.me>
2026-01-28 14:51:25 +0100

    feat: added DialogThemesKotlinView

---
+++ src/main/kotlin/com/webforj/samples/views/dialog/DialogThemesKotlinView.kt
@@ -0,0 +1,42 @@
+package com.webforj.samples.views.dialog
+
+import com.webforj.component.Composite
+import com.webforj.component.Theme
+import com.webforj.component.html.elements.Div
+import com.webforj.component.layout.flexlayout.FlexLayout
+import com.webforj.kotlin.dsl.component.dialog.addToHeader
+import com.webforj.kotlin.dsl.component.dialog.dialog
+import com.webforj.kotlin.dsl.component.html.elements.div
+import com.webforj.kotlin.dsl.component.html.elements.header
+import com.webforj.kotlin.dsl.component.list.choiceBox
+import com.webforj.kotlin.extension.set
+import com.webforj.kotlin.extension.styles
+import com.webforj.router.annotation.FrameTitle
+import com.webforj.router.annotation.Route
+
+@Route
+@FrameTitle("Dialog Themes")
+class DialogThemesKotlinView: Composite<FlexLayout>() {
+
+  init {
+      boundComponent.apply {
+        dialog {
+          addToHeader { div("Themes") }
+          styles["display"] = "flex"
+          styles["justify-content"] = "center"
+          choiceBox("Select Theme") {
+            styles["flex"] = "1"
+            Theme.entries.forEach {
+              add(it, it.name)
+            }
+            selectIndex(1)
+            onSelect {
+              this@dialog.theme = it.selectedItem.key as Theme
+            }
+          }
+          open()
+          setCloseable(false)
+        }
+      }
+  }
+}
