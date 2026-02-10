2d91e6eaa5b5a812e550b9ed8896ef76a10312eb
cliebhardt <liebhardt.christian@proton.me>
2026-02-10 19:10:54 +0100

    refactor: use DSL features in Button Views

--- src/main/kotlin/com/webforj/samples/views/button/ButtonIconKotlinView.kt
+++ src/main/kotlin/com/webforj/samples/views/button/ButtonIconKotlinView.kt
@@ -4,12 +4,15 @@ import com.webforj.component.Composite
 import com.webforj.component.layout.flexlayout.FlexLayout
 import com.webforj.kotlin.dsl.component.button.button
 import com.webforj.kotlin.dsl.component.button.icon
-import com.webforj.kotlin.dsl.component.button.prefix
-import com.webforj.kotlin.dsl.component.button.suffix
 import com.webforj.kotlin.dsl.component.html.elements.img
 import com.webforj.kotlin.dsl.component.icons.tablerIcon
+import com.webforj.kotlin.extension.percent
+import com.webforj.kotlin.extension.prefix
+import com.webforj.kotlin.extension.px
 import com.webforj.kotlin.extension.set
+import com.webforj.kotlin.extension.size
 import com.webforj.kotlin.extension.styles
+import com.webforj.kotlin.extension.suffix
 import com.webforj.router.annotation.FrameTitle
 import com.webforj.router.annotation.Route
 
@@ -23,7 +26,7 @@ class ButtonIconKotlinView: Composite<FlexLayout>() {
         spacing = "var(--dwc-space-l)"
         margin = "var(--dwc-space-l)"
         styles["flex-wrap"] = "wrap"
-        width = "100%"
+        width = 100.percent
         button("Notifications") {
           prefix { tablerIcon("bell") }
         }
@@ -33,7 +36,7 @@ class ButtonIconKotlinView: Composite<FlexLayout>() {
         button {
           icon {
             img("https://documentation.webforj.com/img/webforj.svg") {
-              setSize("100px", "30px")
+              size = 100.px to 30.px
             }
           }
         }
