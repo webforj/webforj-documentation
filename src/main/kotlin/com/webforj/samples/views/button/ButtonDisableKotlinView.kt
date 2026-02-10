408ca21390562e9ce31b80e7c13318b9a3982344
cliebhardt <liebhardt.christian@proton.me>
2026-02-10 19:10:54 +0100

    refactor: use DSL features in Button Views

--- src/main/kotlin/com/webforj/samples/views/button/ButtonDisableKotlinView.kt
+++ src/main/kotlin/com/webforj/samples/views/button/ButtonDisableKotlinView.kt
@@ -7,6 +7,7 @@ import com.webforj.component.layout.flexlayout.FlexAlignment
 import com.webforj.component.layout.flexlayout.FlexLayout
 import com.webforj.kotlin.dsl.component.button.button
 import com.webforj.kotlin.dsl.component.field.textField
+import com.webforj.kotlin.extension.percent
 import com.webforj.kotlin.extension.set
 import com.webforj.kotlin.extension.styles
 import com.webforj.router.annotation.FrameTitle
@@ -23,7 +24,7 @@ class ButtonDisableKotlinView: Composite<FlexLayout>() {
         margin = "var(--dwc-space-l)"
         spacing = "var(--dwc-space-xl)"
         styles["flex-wrap"] = "wrap"
-        width = "100%"
+        width = 100.percent
         val email = textField("Enter an email", type = Type.EMAIL)
         button("Submit", ButtonTheme.PRIMARY) {
           isEnabled = false
