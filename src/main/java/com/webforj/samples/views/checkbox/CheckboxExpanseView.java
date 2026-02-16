3be76e554aeb1b8bd9a99620375de6fdd10e12c5
cliebhardt <liebhardt.christian@gmail.com>
2026-02-16 18:23:33 +0100

    refactor: improve CheckboxExpandView
    - introduce self field for boundComponent
    - stack chain method call
    - use Stream based CheckBox creation

--- src/main/java/com/webforj/samples/views/checkbox/CheckboxExpanseView.java
+++ src/main/java/com/webforj/samples/views/checkbox/CheckboxExpanseView.java
@@ -9,17 +9,24 @@ import com.webforj.component.optioninput.CheckBox;
 import com.webforj.router.annotation.FrameTitle;
 import com.webforj.router.annotation.Route;
 
+import java.util.Arrays;
+
 @Route
 @FrameTitle("Checkbox Expanses")
 public class CheckboxExpanseView extends Composite<FlexLayout> {
+  private FlexLayout self = getBoundComponent();
 
   public CheckboxExpanseView() {
-    getBoundComponent().setWrap(FlexWrap.WRAP).setMargin("var(--dwc-space-l)").setSpacing("50px")
-    .setJustifyContent(FlexJustifyContent.CENTER).setWidth("100%");
+    self.setWrap(FlexWrap.WRAP)
+            .setMargin("var(--dwc-space-l)")
+            .setSpacing("50px")
+            .setJustifyContent(FlexJustifyContent.CENTER)
+            .setWidth("100%");
 
-    for (int i = Expanse.values().length - 1; i >= 0; i--) {
-      CheckBox expanseCheckBox = new CheckBox(Expanse.values()[i].name());
-      getBoundComponent().add(expanseCheckBox.setExpanse(Expanse.values()[i]));
-    }
+    Arrays.asList(Expanse.values())
+            .reversed()
+            .stream()
+            .map(expanse -> new CheckBox(expanse.name()).setExpanse(expanse))
+            .forEach(self::add);
   }
 }
