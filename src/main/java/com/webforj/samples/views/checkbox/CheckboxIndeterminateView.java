934f8f3c779073bea7f4989fa91e3c6c716189cd
cliebhardt <liebhardt.christian@gmail.com>
2026-02-16 18:16:03 +0100

    refactor: improved CheckboxIndeterminateView
    - change field visbility to private
    - introduce self field for boundComponent
    - use fluent setter API
    - simplify event handling logic

--- src/main/java/com/webforj/samples/views/checkbox/CheckboxIndeterminateView.java
+++ src/main/java/com/webforj/samples/views/checkbox/CheckboxIndeterminateView.java
@@ -11,61 +11,49 @@ import com.webforj.router.annotation.Route;
 @Route
 @FrameTitle("Checkbox Indeterminate")
 public class CheckboxIndeterminateView extends Composite<FlexLayout> {
-
-  CheckBox indeterminate = new CheckBox("Parent");
-  CheckBox child1 = new CheckBox("Child 1");
-  CheckBox child2 = new CheckBox("Child 2");
+  private FlexLayout self = getBoundComponent();
+  private CheckBox indeterminate = new CheckBox("Parent");
+  private CheckBox child1 = new CheckBox("Child 1");
+  private CheckBox child2 = new CheckBox("Child 2");
 
   public CheckboxIndeterminateView() {
-    getBoundComponent().setDirection(FlexDirection.COLUMN).setMargin("var(--dwc-space-l)");
-
-    indeterminate.setIndeterminate(true);
-    indeterminate.addToggleListener(this::indeterminateToggle);
+    indeterminate.setIndeterminate(true)
+            .addToggleListener(this::indeterminateToggle);
 
     FlexLayout firstRow = FlexLayout.create(indeterminate)
-        .horizontal()
-        .build();
-    
+            .horizontal()
+            .build();
+
     child1.setChecked(false)
-        .addToggleListener(this::onCheck);
-    
+            .addToggleListener(this::onCheck);
+
     FlexLayout secondRow = FlexLayout.create(child1)
-        .horizontal()
-        .build()
-        .setStyle("margin-left", "30px");
+            .horizontal()
+            .build()
+            .setStyle("margin-left", "30px");
 
     child2.setChecked(true)
-        .addToggleListener(this::onCheck);
-    
-    FlexLayout thirdRow = FlexLayout.create(child2)
-        .horizontal()
-        .build()
-        .setStyle("margin-left", "30px");;
+            .addToggleListener(this::onCheck);
 
-    getBoundComponent().add(firstRow, secondRow, thirdRow);
+    FlexLayout thirdRow = FlexLayout.create(child2)
+            .horizontal()
+            .build()
+            .setStyle("margin-left", "30px");
+    ;
+
+    self.setDirection(FlexDirection.COLUMN)
+            .setMargin("var(--dwc-space-l)")
+            .add(firstRow, secondRow, thirdRow);
   }
 
   private void onCheck(ToggleEvent e) {
-    if (child1.isChecked() && child2.isChecked()) {
-      indeterminate.setChecked(true);
-    } else {
-      if (child1.isChecked() || child2.isChecked()) {
-        indeterminate.setChecked(false);
-        indeterminate.setIndeterminate(true);
-      } else {
-        indeterminate.setIndeterminate(false);
-        indeterminate.setChecked(false);
-      }
-    }
+    indeterminate.setChecked(child1.isChecked() && child2.isChecked());
+    indeterminate.setIndeterminate(child1.isIndeterminate() ^ child2.isChecked());
   }
 
   private void indeterminateToggle(ToggleEvent e) {
-    if (e.isToggled()) {
-      child1.setChecked(true);
-      child2.setChecked(true);
-    } else {
-      child1.setChecked(false);
-      child2.setChecked(false);
-    }
+    boolean toggled = e.isToggled();
+    child1.setChecked(toggled);
+    child2.setChecked(toggled);
   }
 }
