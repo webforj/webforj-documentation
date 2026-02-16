b8bee72dde016bb4d4ebc5aa11ec047587623077
cliebhardt <liebhardt.christian@gmail.com>
2026-02-16 17:06:40 +0100

    refactor: improve BusyIndicatorView
    - introduce indicator field
    - move event handling out of constructor

--- src/main/java/com/webforj/samples/views/busyindicator/BusyIndicatorView.java
+++ src/main/java/com/webforj/samples/views/busyindicator/BusyIndicatorView.java
@@ -15,37 +15,39 @@ import com.webforj.router.annotation.Route;
 @FrameTitle("Busy Indicator")
 public class BusyIndicatorView extends Composite<Div> {
   private int retry = 1;
+  private BusyIndicator indicator = App.getBusyIndicator();
 
   public BusyIndicatorView() {
 
     // Advance configurations
-    BusyIndicator indicator = App.getBusyIndicator();
     indicator.setText("Initiating spoon pending sequence... Hold tight! This will take 3 seconds.");
 
-    Interval interval = new Interval(3f, new EventListener<Interval.ElapsedEvent>() {
-      @Override
-      public void onEvent(Interval.ElapsedEvent event) {
-        if (retry <= 1) {
-          indicator.getSpinner().setTheme(Theme.WARNING).setSpeed(200)
-              .setExpanse(SpinnerExpanse.LARGE);
-          indicator.setText(
-              "Attempt 1: Still trying to bend the spoon... It's trickier than it looks!, Trying harder...");
-        } else if (retry == 2) {
-          indicator.getSpinner().setTheme(Theme.DANGER);
-          indicator.setText(
-              "Attempt 2: Spoon bending failed... Maybe it’s magic-proof. Let's stop here.");
-        } else {
-          event.getInterval().stop();
-          indicator.getSpinner().setTheme(Theme.SUCCESS);
-          indicator.setText(
-              "Demo complete! Remember, it's not about bending spoons, it's about having fun!");
-        }
-
-        retry++;
-      }
-    });
+    Interval interval = new Interval(3f, this::handleElapsedEvent);
 
     indicator.open();
     interval.start();
+  }
+
+  private void handleElapsedEvent(Interval.ElapsedEvent event) {
+    switch (retry) {
+      case 1:
+        indicator.getSpinner().setTheme(Theme.WARNING).setSpeed(200)
+                .setExpanse(SpinnerExpanse.LARGE);
+        indicator.setText(
+                "Attempt 1: Still trying to bend the spoon... It's trickier than it looks!, Trying harder...");
+        break;
+      case 2:
+        indicator.getSpinner().setTheme(Theme.DANGER);
+        indicator.setText(
+                "Attempt 2: Spoon bending failed... Maybe it’s magic-proof. Let's stop here.");
+        break;
+      default:
+        event.getInterval().stop();
+        indicator.getSpinner().setTheme(Theme.SUCCESS);
+        indicator.setText(
+                "Demo complete! Remember, it's not about bending spoons, it's about having fun!");
+    }
+
+    retry++;
   }
 }
