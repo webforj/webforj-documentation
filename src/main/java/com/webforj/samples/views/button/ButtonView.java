a56610120cf10a062777a15d30895f5d1d0bd2df
cliebhardt <liebhardt.christian@gmail.com>
2026-02-16 17:19:08 +0100

    refactor: improve ButtonView
    - introduce self field
    - change field visbility  to private
    - move row setup to dedicated methods

--- src/main/java/com/webforj/samples/views/button/ButtonView.java
+++ src/main/java/com/webforj/samples/views/button/ButtonView.java
@@ -19,57 +19,76 @@ import com.webforj.router.annotation.Route;
 @Route
 @FrameTitle("Button Demo")
 public class ButtonView extends Composite<FlexLayout> {
-
-  Button clear;
-  Button submit;
-  TextField firstName;
-  TextField lastName;
-  TextField email;
+  private FlexLayout self = getBoundComponent();
+  private Button clear;
+  private Button submit;
+  private TextField firstName;
+  private TextField lastName;
+  private TextField email;
 
   public ButtonView() {
+    self.setDirection(FlexDirection.COLUMN)
+            .setSpacing("var(--dwc-space-l)")
+            .setAlignment(FlexAlignment.CENTER)
+            .setMargin("var(--dwc-space-l)");
+    setupFirstRow();
+    setupSecondRow();
+    setupThirdRow();
+  }
 
-    getBoundComponent().setDirection(FlexDirection.COLUMN).setSpacing("var(--dwc-space-l)")
-        .setAlignment(FlexAlignment.CENTER).setMargin("var(--dwc-space-l)");
+  private void setupFirstRow() {
+    firstName = new TextField(Type.TEXT, "First Name", "Jason")
+            .setWidth("25%")
+            .setStyle("flex", "1");
+    lastName = new TextField(Type.TEXT, "Last Name", "Turner")
+            .setWidth("25%")
+            .setStyle("flex", "1");
+
+    FlexLayout firstRow = FlexLayout.create(firstName, lastName)
+            .justify().center()
+            .wrap()
+            .build()
+            .setSpacing("var(--dwc-space-l)")
+            .setWidth("50%")
+            .addClassName("row");
+
+    self.add(firstRow);
+  }
+
+  private void setupSecondRow() {
+    email = new TextField(Type.EMAIL, "E-mail:", "turner.jason@email.com")
+            .setWidth("100%");
+
+    FlexLayout secondRow = FlexLayout.create(email)
+            .justify().center()
+            .wrap()
+            .build()
+            .setSpacing("var(--dwc-space-l)")
+            .setWidth("50%")
+            .addClassName("row");
+
+    self.add(secondRow);
+  }
+
+  private void setupThirdRow() {
+    submit = new Button("Submit", ButtonTheme.PRIMARY, e -> {
+      String message = "Welcome to the app %s %s!".formatted(firstName.getText(), lastName.getText());
+      showMessageDialog(message, "Welcome");
+    });
     clear = new Button("Clear", ButtonTheme.DEFAULT, e -> {
       firstName.setText("");
       lastName.setText("");
       email.setText("");
     });
 
-    submit = new Button("Submit", ButtonTheme.PRIMARY, e -> {
-      showMessageDialog("Welcome to the app " + firstName.getText() + " " + lastName.getText() + "!", "Welcome");
-    });
-
-    firstName = new TextField(Type.TEXT, "First Name", "Jason").setWidth("25%");
-    firstName.setStyle("flex", "1");
-    lastName = new TextField(Type.TEXT, "Last Name", "Turner").setWidth("25%");
-    lastName.setStyle("flex", "1");
-    email = new TextField(Type.EMAIL, "E-mail:", "turner.jason@email.com").setWidth("100%");
-
-    FlexLayout firstRow = FlexLayout.create(firstName, lastName)
-        .justify().center()
-        .wrap()
-        .build()
-        .setSpacing("var(--dwc-space-l)")
-        .setWidth("50%")
-        .addClassName("row");
-
-    FlexLayout secondRow = FlexLayout.create(email)
-        .justify().center()
-        .wrap()
-        .build()
-        .setSpacing("var(--dwc-space-l)")
-        .setWidth("50%")
-        .addClassName("row");
-
     FlexLayout thirdRow = FlexLayout.create(submit, clear)
-        .justify().end()
-        .wrap()
-        .build()
-        .setSpacing("var(--dwc-space-l)")
-        .setWidth("50%")
-        .addClassName("row", "buttons");
+            .justify().end()
+            .wrap()
+            .build()
+            .setSpacing("var(--dwc-space-l)")
+            .setWidth("50%")
+            .addClassName("row", "buttons");
 
-    getBoundComponent().add(firstRow, secondRow, thirdRow);
+    self.add(thirdRow);
   }
 }
