56c05fad1a1acf53c6606ea16d4cc0464d119ef1
cliebhardt <liebhardt.christian@gmail.com>
2026-02-16 16:59:28 +0100

    refactor: BusySpinnerDemo
    - change visiblity of fields to private
    - add updateIndicator method to set theme, expanse and text
    - move event handling to dedicated method

--- src/main/java/com/webforj/samples/views/busyindicator/BusySpinnerDemoView.java
+++ src/main/java/com/webforj/samples/views/busyindicator/BusySpinnerDemoView.java
@@ -4,6 +4,7 @@ import com.webforj.App;
 import com.webforj.BusyIndicator;
 import com.webforj.Interval;
 import com.webforj.component.Composite;
+import com.webforj.component.Expanse;
 import com.webforj.component.Theme;
 import com.webforj.component.html.elements.Div;
 import com.webforj.component.spinner.SpinnerExpanse;
@@ -17,62 +18,70 @@ import com.webforj.router.annotation.Route;
 @Route
 @FrameTitle("Busy Spinners")
 public class BusySpinnerDemoView extends Composite<Div> {
-
-  int state = 1;  
-  BusyIndicator indicator;
-  Interval interval;
+  private int state = 1;
+  private BusyIndicator indicator;
+  private Interval interval;
   
   public BusySpinnerDemoView() {
-    
     indicator  = App.getBusyIndicator().setText("Displaying spinner with all themes...");
 
-    interval = new Interval(3f, event -> {
-            switch (state) {
-              case 1:
-                indicator.getSpinner().setTheme(Theme.DEFAULT).setExpanse(SpinnerExpanse.SMALL);
-                indicator.setText("Displaying default theme, small expanse");
-                break;
-              case 2:
-                indicator.getSpinner().setTheme(Theme.DANGER).setExpanse(SpinnerExpanse.MEDIUM);
-                indicator.setText("Displaying danger theme, medium expanse");
-                break;
-              case 3:
-                indicator.getSpinner().setTheme(Theme.GRAY).setExpanse(SpinnerExpanse.LARGE);
-                indicator.setText("Displaying gray theme, large expanse");
-                break;
-              case 4:
-                indicator.getSpinner().setTheme(Theme.INFO).setExpanse(SpinnerExpanse.SMALL);
-                indicator.setText("Displaying info theme, small expanse");
-                break;
-              case 5:
-                indicator.getSpinner().setTheme(Theme.PRIMARY).setExpanse(SpinnerExpanse.MEDIUM);
-                indicator.setText("Displaying primary theme, medium expanse");
-                break;
-              case 6:
-                indicator.getSpinner().setTheme(Theme.SUCCESS).setExpanse(SpinnerExpanse.LARGE);
-                indicator.setText("Displaying success theme, large expanse");
-                break;
-              case 7:
-                indicator.getSpinner().setTheme(Theme.WARNING).setExpanse(SpinnerExpanse.SMALL);
-                indicator.setText("Displaying warning theme, small expanse");
-                break;
-              case 8:
-                indicator.getSpinner().setClockwise(false);
-                indicator.setText("Now moving counterclockwise...");
-                break;
-              case 9:
-                indicator.getSpinner().setSpeed(500);
-                indicator.setText("Going faster...");
-                break;
-              default:
-                event.getInterval().stop();
-                indicator.getSpinner().setTheme(Theme.PRIMARY);
-                indicator.setText("Demo complete!");
-            }
-            state++;
-        });
+    interval = new Interval(3f, this::handleElapsedEvent);
 
     indicator.open();
     interval.start();
+  }
+
+  private void handleElapsedEvent(Interval.ElapsedEvent event) {
+    switch (state) {
+      case 1:
+        updateIndicator(Theme.DEFAULT, SpinnerExpanse.SMALL,
+                "Displaying default theme, small expanse");
+        break;
+      case 2:
+        updateIndicator(Theme.DANGER, SpinnerExpanse.MEDIUM,
+                "Displaying danger theme, medium expanse");
+        break;
+      case 3:
+        updateIndicator(Theme.GRAY, SpinnerExpanse.LARGE,
+                "Displaying gray theme, large expanse");
+        break;
+      case 4:
+        updateIndicator(Theme.INFO, SpinnerExpanse.SMALL,
+                "Displaying info theme, small expanse");
+        break;
+      case 5:
+        updateIndicator(Theme.PRIMARY, SpinnerExpanse.MEDIUM,
+                "Displaying primary theme, medium expanse");
+        break;
+      case 6:
+        updateIndicator(Theme.SUCCESS, SpinnerExpanse.LARGE,
+                "Displaying success theme, large expanse");
+        break;
+      case 7:
+        updateIndicator(Theme.WARNING, SpinnerExpanse.SMALL,
+                "Displaying warning theme, small expanse");
+        break;
+      case 8:
+        indicator.getSpinner().setClockwise(false);
+        indicator.setText("Now moving counterclockwise...");
+        break;
+      case 9:
+        indicator.getSpinner().setSpeed(500);
+        indicator.setText("Going faster...");
+        break;
+      default:
+        event.getInterval().stop();
+        indicator.getSpinner().setTheme(Theme.PRIMARY);
+        indicator.setText("Demo complete!");
+    }
+    state++;
+  }
+
+  private void updateIndicator(Theme theme, SpinnerExpanse expanse, String text) {
+    indicator
+            .setText(text)
+            .getSpinner()
+            .setTheme(theme)
+            .setExpanse(expanse);
   }
 }
