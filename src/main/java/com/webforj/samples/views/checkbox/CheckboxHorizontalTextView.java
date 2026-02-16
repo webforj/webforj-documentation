6c9812569bf7053183b5dfc3c4ae06978f3f420b
cliebhardt <liebhardt.christian@gmail.com>
2026-02-16 18:19:37 +0100

    refactor: improve CheckboxHorizontalTextView
    - introduce self field for boundComponent
    - move Checkbox creation into FlexLayout initalization

--- src/main/java/com/webforj/samples/views/checkbox/CheckboxHorizontalTextView.java
+++ src/main/java/com/webforj/samples/views/checkbox/CheckboxHorizontalTextView.java
@@ -12,35 +12,36 @@ import com.webforj.router.annotation.Route;
 @Route
 @FrameTitle("Checkbox Horizontal Text")
 public class CheckboxHorizontalTextView extends Composite<FlexLayout> {
+  private FlexLayout self = getBoundComponent();
 
   public CheckboxHorizontalTextView() {
-    getBoundComponent().setDirection(FlexDirection.ROW).setWrap(FlexWrap.WRAP).setSpacing("var(--dwc-space-l)")
-        .setMargin("var(--dwc-space-l)");
-    FlexLayout rightAligned = FlexLayout.create()
-        .vertical()
-        .build()
-        .setWidth("100px");
+    FlexLayout rightAligned = FlexLayout.create(
+                    new CheckBox("Daily", true),
+                    new CheckBox("Weekly"),
+                    new CheckBox("Bi-Weekly"),
+                    new CheckBox("Monthly"),
+                    new CheckBox("Annually")
+            )
+            .vertical()
+            .build()
+            .setWidth("100px");
 
-    FlexLayout leftAligned = FlexLayout.create()
-        .vertical()
-        .align().end()
-        .build()
-        .setWidth("100px");
+    FlexLayout leftAligned = FlexLayout.create(
+                    new CheckBox("Daily", true).setTextPosition(Position.LEFT),
+                    new CheckBox("Weekly").setTextPosition(Position.LEFT),
+                    new CheckBox("Bi-Weekly").setTextPosition(Position.LEFT),
+                    new CheckBox("Monthly").setTextPosition(Position.LEFT),
+                    new CheckBox("Annually").setTextPosition(Position.LEFT)
+            )
+            .vertical()
+            .align().end()
+            .build()
+            .setWidth("100px");
 
-    rightAligned.add(
-        new CheckBox("Daily", true),
-        new CheckBox("Weekly"),
-        new CheckBox("Bi-Weekly"),
-        new CheckBox("Monthly"),
-        new CheckBox("Annually"));
-
-    leftAligned.add(
-        new CheckBox("Daily", true).setTextPosition(Position.LEFT),
-        new CheckBox("Weekly").setTextPosition(Position.LEFT),
-        new CheckBox("Bi-Weekly").setTextPosition(Position.LEFT),
-        new CheckBox("Monthly").setTextPosition(Position.LEFT),
-        new CheckBox("Annually").setTextPosition(Position.LEFT));
-
-    getBoundComponent().add(rightAligned, leftAligned);
+    self.setDirection(FlexDirection.ROW)
+            .setWrap(FlexWrap.WRAP)
+            .setSpacing("var(--dwc-space-l)")
+            .setMargin("var(--dwc-space-l)")
+            .add(rightAligned, leftAligned);
   }
 }
