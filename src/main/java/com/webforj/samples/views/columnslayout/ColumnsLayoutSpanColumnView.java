ff47e1baf3d6efef14acbb99f5cbec27cdb0e131
cliebhardt <liebhardt.christian@gmail.com>
2026-02-16 18:30:04 +0100

    refactor: improve ColumnsLayoutSpanColumnView
    - introduce self field for boundComponent
    - change field visibility to private

--- src/main/java/com/webforj/samples/views/columnslayout/ColumnsLayoutSpanColumnView.java
+++ src/main/java/com/webforj/samples/views/columnslayout/ColumnsLayoutSpanColumnView.java
@@ -13,31 +13,32 @@ import java.util.List;
 @Route
 @FrameTitle("Columns Layout Spans")
 public class ColumnsLayoutSpanColumnView extends Composite<Div> {
-  TextField firstName = new TextField("First Name");
-  TextField lastName = new TextField("Last Name");
-  TextField email = new TextField("Email");
-  PasswordField password = new PasswordField("Password");
-  PasswordField confirmPassword = new PasswordField("Confirm Password");
+  private Div self = getBoundComponent();
+  private TextField firstName = new TextField("First Name");
+  private TextField lastName = new TextField("Last Name");
+  private TextField email = new TextField("Email");
+  private PasswordField password = new PasswordField("Password");
+  private PasswordField confirmPassword = new PasswordField("Confirm Password");
   /**
    * The layout will position the components in 3 columns by default.
    * on medium screens, it will span 2 the email field 2 columns.
    */
   ColumnsLayout columnsLayout = new ColumnsLayout(
-      List.of(
-          new ColumnsLayout.Breakpoint("default", 0, 1),
-          new ColumnsLayout.Breakpoint("small", "20em", 1),
-          new ColumnsLayout.Breakpoint("medium", "40em", 2),
-          new ColumnsLayout.Breakpoint("large", "60em", 3)),
-      firstName, lastName, email, password, confirmPassword);
+          List.of(
+                  new ColumnsLayout.Breakpoint("default", 0, 1),
+                  new ColumnsLayout.Breakpoint("small", "20em", 1),
+                  new ColumnsLayout.Breakpoint("medium", "40em", 2),
+                  new ColumnsLayout.Breakpoint("large", "60em", 3)),
+          firstName, lastName, email, password, confirmPassword);
 
   public ColumnsLayoutSpanColumnView() {
-    columnsLayout.setSpan(email, "medium", 2);
-    columnsLayout.setStyle("padding", "var(--dwc-space-xl)");
+    columnsLayout.setSpan(email, "medium", 2)
+            .setStyle("padding", "var(--dwc-space-xl)");
 
-    getBoundComponent().setMaxWidth("60em");
-    getBoundComponent().setStyle("margin", "0 auto");
-    getBoundComponent().setStyle("overflow", "auto");
-    getBoundComponent().setStyle("height", "100dvh");
-    getBoundComponent().add(columnsLayout);
+    self.setMaxWidth("60em")
+            .setStyle("margin", "0 auto")
+            .setStyle("overflow", "auto")
+            .setStyle("height", "100dvh")
+            .add(columnsLayout);
   }
 }
