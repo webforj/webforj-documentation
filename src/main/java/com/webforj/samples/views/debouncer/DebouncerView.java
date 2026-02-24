e625aec1aa2f101e0178c6cca217d8bf7bada94d
cliebhardt <liebhardt.christian@gmail.com>
2026-02-24 10:49:18 +0100

    refacotr: improve DebouncerView by using fluent setter API

--- src/main/java/com/webforj/samples/views/debouncer/DebouncerView.java
+++ src/main/java/com/webforj/samples/views/debouncer/DebouncerView.java
@@ -12,7 +12,6 @@ import com.webforj.router.annotation.Route;
 @Route
 @FrameTitle("Debouncer Demo")
 public class DebouncerView extends Composite<FlexLayout> {
-
   private final FlexLayout self = getBoundComponent();
   private final TextField input = new TextField();
   private final TextArea output = new TextArea();
@@ -21,29 +20,28 @@ public class DebouncerView extends Composite<FlexLayout> {
 
   public DebouncerView() {
     self.setDirection(FlexDirection.COLUMN)
-        .setMaxWidth("400px")
-        .setMargin("var(--dwc-space-xl) auto")
-        .setPadding("var(--dwc-space-l)")
-        .setSpacing("var(--dwc-space-l)");
+            .setMaxWidth("400px")
+            .setMargin("var(--dwc-space-xl) auto")
+            .setPadding("var(--dwc-space-l)")
+            .setSpacing("var(--dwc-space-l)")
+            .add(input, output);
 
-    input.setLabel("Type something");
-    input.setPlaceholder("Start typing...");
-    input.setHelperText("Key events: 0");
-    input.onModify(e -> {
-      count++;
-      input.setHelperText("Key events: " + count);
+    input.setLabel("Type something")
+            .setPlaceholder("Start typing...")
+            .setHelperText("Key events: 0")
+            .onModify(e -> {
+              count++;
+              input.setHelperText("Key events: " + count);
 
-      debouncer.run(() -> {
-        output.setValue(e.getText());
-        input.setHelperText("Key events: 0");
-        count = 0;
-      });
-    });
+              debouncer.run(() -> {
+                output.setValue(e.getText());
+                input.setHelperText("Key events: 0");
+                count = 0;
+              });
+            });
 
-    output.setLabel("Debounced output");
-    output.setReadOnly(true);
-
-    self.add(input, output);
+    output.setLabel("Debounced output")
+            .setReadOnly(true);
   }
 
   @Override
