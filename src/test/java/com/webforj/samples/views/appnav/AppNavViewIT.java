6252c00138bc9d5c07eaa10599be52a7c5c7d3e9
cliebhardt <liebhardt.christian@gmail.com>
2026-03-03 11:12:40 +0100

    test: update AppNav tests to kotlin integration

--- src/test/java/com/webforj/samples/views/appnav/AppNavViewIT.java
+++ src/test/java/com/webforj/samples/views/appnav/AppNavViewIT.java
@@ -2,24 +2,28 @@ package com.webforj.samples.views.appnav;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
+import com.webforj.samples.pages.SupportedLanguage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.webforj.samples.pages.appnav.AppNavPage;
 import com.webforj.samples.views.BaseTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class AppNavViewIT extends BaseTest {
 
     private AppNavPage appNavPage;
 
-    @BeforeEach
-    public void setupAppNav() {
-        navigateToRoute(AppNavPage.getRoute());
+    public void setupAppNav(SupportedLanguage language) {
+        navigateToRoute(AppNavPage.getRoute(language));
         appNavPage = new AppNavPage(page);
     }
 
-    @Test
-    public void testDrawerOpensAndCloses() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testDrawerOpensAndCloses(SupportedLanguage language) {
+        setupAppNav(language);
         appNavPage.getTablerIcon().click();
 
         assertThat(appNavPage.getAppLayout()).not().hasAttribute("drawer-opened", "12");
@@ -28,8 +32,10 @@ public class AppNavViewIT extends BaseTest {
         assertThat(appNavPage.getAppLayout()).hasAttribute("drawer-opened", "");
     }
 
-    @Test
-    public void testPrimaryTabSelected() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testPrimaryTabSelected(SupportedLanguage language) {
+        setupAppNav(language);
         appNavPage.getInboxDropdown().click();
 
         appNavPage.getSidebarPrimaryTab().click();
@@ -37,8 +43,10 @@ public class AppNavViewIT extends BaseTest {
 
     }
 
-    @Test
-    public void testExternalLinksAreCorrect() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testExternalLinksAreCorrect(SupportedLanguage language) {
+        setupAppNav(language);
         appNavPage.getAboutDropdown().click();
 
         assertThat(appNavPage.getSidebarWebforJ().locator("a")).hasAttribute("href", "https://webforj.com/");
