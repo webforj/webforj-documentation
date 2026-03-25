7073f4a1966b09628055d919d396fe9c0c03ce6c
Hyyan Abo Fakher <hyyanaf@gmail.com>
2026-03-25 05:38:46 +0100

    refactor: remove unused variable in BusySpinnerDemoView class

--- src/main/java/com/webforj/samples/views/busyindicator/BusySpinnerDemoView.java
+++ src/main/java/com/webforj/samples/views/busyindicator/BusySpinnerDemoView.java
@@ -17,7 +17,6 @@ import com.webforj.router.annotation.Route;
 @Route
 @FrameTitle("Busy Spinners")
 public class BusySpinnerDemoView extends Composite<Div> {
-  private final Div self = getBoundComponent();
   private int state = 1;
   private final BusyIndicator indicator;
   private final Interval interval;
