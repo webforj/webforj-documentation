98a47d17b2bf76b9ce0bcfa2cdfc67072a8f34e1
cliebhardt <liebhardt.christian@proton.me>
2026-03-30 16:23:10 +0200

    test: update accordion integration tests to include kotlin

--- src/test/java/com/webforj/samples/views/accordion/AccordionCustomHeaderViewIT.java
+++ src/test/java/com/webforj/samples/views/accordion/AccordionCustomHeaderViewIT.java
@@ -2,34 +2,41 @@ package com.webforj.samples.views.accordion;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import com.webforj.samples.pages.SupportedLanguage;
 import com.webforj.samples.pages.accordion.AccordionCustomHeaderPage;
 import com.webforj.samples.views.BaseTest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class AccordionCustomHeaderViewIT extends BaseTest {
 
     private AccordionCustomHeaderPage accordionCustomHeaderPage;
 
-    @BeforeEach
-    public void setupAccordionCustomHeader() {
-        navigateToRoute(AccordionCustomHeaderPage.getRoute());
+    public void setupAccordionCustomHeader(SupportedLanguage language) {
+        navigateToRoute(AccordionCustomHeaderPage.getRoute(language));
         accordionCustomHeaderPage = new AccordionCustomHeaderPage(page);
     }
 
-    @Test
-    public void testCustomHeaderPanelIsVisible() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testCustomHeaderPanelIsVisible(SupportedLanguage language) {
+        setupAccordionCustomHeader(language);
         assertThat(accordionCustomHeaderPage.getCustomHeaderPanel()).isVisible();
     }
 
-    @Test
-    public void testUserSettingsPanelIsVisible() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testUserSettingsPanelIsVisible(SupportedLanguage language) {
+        setupAccordionCustomHeader(language);
         assertThat(accordionCustomHeaderPage.getUserSettingsPanel()).isVisible();
     }
 
-    @Test
-    public void testClickingCustomHeaderPanelOpensIt() {
+    @ParameterizedTest
+    @MethodSource("provideRoutes")
+    public void testClickingCustomHeaderPanelOpensIt(SupportedLanguage language) {
+        setupAccordionCustomHeader(language);
         accordionCustomHeaderPage.getCustomHeaderPanel().click();
         assertThat(accordionCustomHeaderPage.getCustomHeaderPanel()).hasAttribute("opened", "");
     }
