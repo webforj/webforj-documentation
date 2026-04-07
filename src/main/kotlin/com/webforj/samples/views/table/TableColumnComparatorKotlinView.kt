dc490e698d4fc31bdeb6fcc4bce4b467954eecd5
cliebhardt <liebhardt.christian@proton.me>
2026-04-07 22:35:48 +0200

    refactor: use apply block instead of chaining in TableColumnComaratorKotlinView

--- src/main/kotlin/com/webforj/samples/views/table/TableColumnComparatorKotlinView.kt
+++ src/main/kotlin/com/webforj/samples/views/table/TableColumnComparatorKotlinView.kt
@@ -2,13 +2,11 @@ package com.webforj.samples.views.table
 
 import com.webforj.component.Composite
 import com.webforj.component.html.elements.Div
-import com.webforj.component.table.Table
 import com.webforj.kotlin.dsl.component.table.table
 import com.webforj.kotlin.extension.vh
 import com.webforj.kotlin.extension.vw
 import com.webforj.router.annotation.FrameTitle
 import com.webforj.router.annotation.Route
-import kotlin.comparisons.compareBy
 
 @Route
 @FrameTitle("Table Column Comparator")
@@ -21,8 +19,9 @@ class TableColumnComparatorKotlinView : Composite<Div>() {
         repository = Service.getMusicRecords()
         width = 100.vw
         height = 100.vh
-        addColumn("Number", MusicRecord::getNumber)
-          .setComparator(Comparator.comparingInt { it.number.toInt() })
+        addColumn("Number", MusicRecord::getNumber).apply {
+          comparator = Comparator.comparingInt { it.number.toInt() }
+        }
         addColumn("Title", MusicRecord::getTitle)
         addColumn("Artist", MusicRecord::getArtist)
         addColumn("Genre", MusicRecord::getMusicType)
