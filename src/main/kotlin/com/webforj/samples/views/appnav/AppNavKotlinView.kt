b98cf33d30efa8ddbb48853ef69b0bac6bec1cc1
cliebhardt <liebhardt.christian@proton.me>
2026-04-07 23:07:31 +0200

    fix: use correct slot extensions

--- src/main/kotlin/com/webforj/samples/views/appnav/AppNavKotlinView.kt
+++ src/main/kotlin/com/webforj/samples/views/appnav/AppNavKotlinView.kt
@@ -7,18 +7,17 @@ import com.webforj.kotlin.dsl.component.html.elements.h1
 import com.webforj.kotlin.dsl.component.html.elements.strong
 import com.webforj.kotlin.dsl.component.icons.tablerIcon
 import com.webforj.kotlin.dsl.component.layout.applayout.appDrawerToggle
-import com.webforj.kotlin.dsl.component.layout.applayout.drawer
-import com.webforj.kotlin.dsl.component.layout.applayout.header
+import com.webforj.kotlin.dsl.component.layout.applayout.drawerSlot
+import com.webforj.kotlin.dsl.component.layout.applayout.headerSlot
 import com.webforj.kotlin.dsl.component.layout.appnav.appNav
 import com.webforj.kotlin.dsl.component.layout.appnav.appNavItem
-import com.webforj.kotlin.dsl.component.toolbar.start
-import com.webforj.kotlin.dsl.component.toolbar.title
+import com.webforj.kotlin.dsl.component.toolbar.startSlot
+import com.webforj.kotlin.dsl.component.toolbar.titleSlot
 import com.webforj.kotlin.dsl.component.toolbar.toolbar
-import com.webforj.kotlin.extension.prefix
-import com.webforj.kotlin.extension.suffix
+import com.webforj.kotlin.extension.prefixSlot
+import com.webforj.kotlin.extension.suffixSlot
 import com.webforj.router.annotation.Route
 import com.webforj.router.history.ParametersBag
-import javax.security.auth.login.Configuration
 
 @Route
 class AppNavKotlinView: Composite<AppLayout>() {
@@ -28,57 +27,57 @@ class AppNavKotlinView: Composite<AppLayout>() {
     self.apply {
       isHeaderOffscreen = false
       isDrawerHeaderVisible = true
-      header {
+      headerSlot {
         toolbar {
           theme = Theme.PRIMARY
-          start { appDrawerToggle() }
-          title { h1("Application") }
+          startSlot { appDrawerToggle() }
+          titleSlot { h1("Application") }
         }
       }
-      drawer {
+      drawerSlot{
         appNav {
           isAutoOpen = true
           appNavItem("Inbox") {
-            prefix { tablerIcon("inbox") }
-            suffix { strong("54") }
+            prefixSlot { tablerIcon("inbox") }
+            suffixSlot { strong("54") }
             appNavItem("Primary", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Primary")) {
-              prefix { tablerIcon("mailbox") }
+              prefixSlot { tablerIcon("mailbox") }
             }
             appNavItem("Promotions", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Promotions")) {
-              prefix { tablerIcon("tag") }
+              prefixSlot { tablerIcon("tag") }
             }
             appNavItem("Social", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Social")) {
-              prefix { tablerIcon("users") }
+              prefixSlot { tablerIcon("users") }
             }
             appNavItem("Updates", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Updates")) {
-              prefix { tablerIcon("bell") }
+              prefixSlot { tablerIcon("bell") }
             }
             appNavItem("Forums", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Forums")) {
-              prefix { tablerIcon("message-circle") }
+              prefixSlot { tablerIcon("message-circle") }
             }
           }
           appNavItem("Sent", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Sent")) {
-            prefix { tablerIcon("send") }
+            prefixSlot { tablerIcon("send") }
           }
           appNavItem("Archived", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Archived")) {
-            prefix { tablerIcon("archive") }
+            prefixSlot { tablerIcon("archive") }
           }
           appNavItem("Trash", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Trash")) {
-            prefix { tablerIcon("trash") }
+            prefixSlot { tablerIcon("trash") }
           }
           appNavItem("Spam", view = AppNavPageKotlinView::class, routeParameters = ParametersBag.of("id=Spam")) {
-            prefix { tablerIcon("alert-hexagon") }
+            prefixSlot { tablerIcon("alert-hexagon") }
           }
           appNavItem("About") {
-            prefix { tablerIcon("info-circle") }
+            prefixSlot { tablerIcon("info-circle") }
             appNavItem("webforj", "https://webforj.com/") {
-              prefix { tablerIcon("external-link") }
+              prefixSlot { tablerIcon("external-link") }
             }
             appNavItem("GitHub", "https://github.com/webforj/webforj") {
-              prefix { tablerIcon("brand-github") }
+              prefixSlot { tablerIcon("brand-github") }
             }
             appNavItem("Documentation", "https://documentation.webforj.com/") {
-              prefix { tablerIcon("book") }
+              prefixSlot { tablerIcon("book") }
             }
           }
         }
