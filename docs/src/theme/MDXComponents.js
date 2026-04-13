37525da204e300136ff0b4c96c2a21d25a6d71fe
Eric Handtke <ehandtke@basis.cloud>
2026-04-13 11:40:13 +0200

    Fixed missing experimental import.

--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -14,6 +14,7 @@ import GiscusComments from '@site/src/components/DocsTools/GiscusComments';
 import AskMenu from '@site/src/components/DocsTools/AskMenu';
 import TableWrapper from '@site/src/components/DocsTools/TableWrapper';
 import AccordionGroup from '@site/src/components/DocsTools/AccordionGroup';
+import ExperimentalWarning from '@site/src/components/DocsTools/ExperimentalWarning';
 import DocCardList from '@theme/DocCardList';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
@@ -44,6 +45,7 @@ export default {
   GiscusComments,
   AskMenu,
   AccordionGroup,
+  ExperimentalWarning,
   Accordion,
   AccordionSummary,
   AccordionDetails,
