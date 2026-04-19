97b4180f6ee0530f74a79e4e7f371a029b2d7ce8
cliebhardt <liebhardt.christian@proton.me>
2026-04-19 22:46:54 +0200

    fix: use flexlayout instead of div in ProgressBarThemesKotlinVIew

--- src/main/kotlin/com/webforj/samples/views/progressbar/ProgressBarThemesKotlinView.kt
+++ src/main/kotlin/com/webforj/samples/views/progressbar/ProgressBarThemesKotlinView.kt
@@ -2,8 +2,7 @@ package com.webforj.samples.views.progressbar
 
 import com.webforj.component.Composite
 import com.webforj.component.Theme
-import com.webforj.component.html.elements.Div
-import com.webforj.kotlin.dsl.component.layout.flexlayout.flexLayout
+import com.webforj.component.layout.flexlayout.FlexLayout
 import com.webforj.kotlin.dsl.component.layout.flexlayout.vertical
 import com.webforj.kotlin.dsl.component.progressbar.progressBar
 import com.webforj.kotlin.extension.px
@@ -14,27 +13,25 @@ import com.webforj.router.annotation.Route
 
 @Route
 @FrameTitle("Progress Bar Themes")
-class ProgressBarThemesKotlinView: Composite<Div>() {
+class ProgressBarThemesKotlinView : Composite<FlexLayout>() {
   private val self = boundComponent
 
   init {
-      self.apply {
-        flexLayout {
-          vertical()
-          maxWidth = 320.px
-          styles["margin"] = "0 auto"
-          styles["padding"] = 20.px
+    self.apply {
+      vertical()
+      maxWidth = 320.px
+      styles["margin"] = "0 auto"
+      styles["padding"] = 20.px
 
-          Theme.entries.forEach {
-            progressBar {
-              isAnimated = true
-              value = 50
-              isStriped = true
-              text = "${it.name} {{x}}%"
-              theme = it
-            }
-          }
+      Theme.entries.forEach {
+        progressBar {
+          isAnimated = true
+          value = 50
+          isStriped = true
+          text = "${it.name} {{x}}%"
+          theme = it
         }
       }
+    }
   }
 }
