271b05c8c959b2defa1b48343f2f73c2d8e08913
Ben Brennan <bbrennan@basis.cloud>
2026-07-08 14:52:40 -0600

    chore: Review edits for PR #1009 - Removed unused parts in client component docs (imports, br tags, etc.) - Doesn't load a DocChip if the client component doesn't have encapsulation

--- docs/docs/client-components/create_files.js
+++ docs/docs/client-components/create_files.js
@@ -52,23 +52,16 @@ async function fetchData() {
         || `Style the ${tag} client component with CSS variables, shadow parts, and slots for theming.`;
 
       const markdownContent = `---
-sidebar_position: 0
 title: <${tag}>
 sidebar_class_name: sidebar--item__hidden
 slug: ${formattedTagName}
 description: ${description}
-// pagination_prev: null
-// pagination_next: null
 ---
-
-import TableBuilder from '@site/src/components/DocsTools/TableBuilder';
-import FiberSmartRecordIcon from '@mui/icons-material/FiberSmartRecord';
-import DocChip from '@site/src/components/DocsTools/DocChip';
-
+${encapsulation !== 'none'
+  ? `
 <DocChip chip='${encapsulation}' />
-
-<br />
-
+    `
+  : ''}
 :::info CLIENT COMPONENT
 This section outlines styling information for the **\`<${tag}>\`** component. This component is **client side only** - it can't be instantiated on its own via the API, but may make up part of API components.
 :::
