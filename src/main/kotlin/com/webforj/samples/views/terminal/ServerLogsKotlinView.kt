98a1106c5d4fed6a27bb3279805e0b987c32a5fe
cliebhardt <liebhardt.christian@gmail.com>
2026-07-15 11:01:03 +0200

    fix: resolve problems in ServerLogsKotlinView
    - swap stop/startStreaming call in if branches
    - correct typo for pause and play icons
    - add missing space between timestamp and level
    - interpolate escape sequence

--- src/main/kotlin/com/webforj/samples/views/terminal/ServerLogsKotlinView.kt
+++ src/main/kotlin/com/webforj/samples/views/terminal/ServerLogsKotlinView.kt
@@ -45,9 +45,9 @@ class ServerLogsKotlinView : Composite<FlexLayout>() {
         prefixSlot { tablerIcon("player-play") }
         onClick {
           if (isStreaming) {
-            startStreaming()
-          } else {
             stopStreaming()
+          } else {
+            startStreaming()
           }
         }
       }
@@ -69,7 +69,7 @@ class ServerLogsKotlinView : Composite<FlexLayout>() {
     isStreaming = true
     startButton.apply {
       text = "Stop Log Stream"
-      prefixSlot { tablerIcon("placer-pause") }
+      prefixSlot { tablerIcon("player-pause") }
     }
     terminal.writeln("\u001B[42;30m  Log streaming started...  \u001B[0m")
     interval.start()
@@ -79,7 +79,7 @@ class ServerLogsKotlinView : Composite<FlexLayout>() {
     isStreaming = false
     startButton.apply {
       text = "Start Log Stream"
-      prefixSlot { tablerIcon("placer-play") }
+      prefixSlot { tablerIcon("player-play") }
       interval.stop()
       terminal.writeln("\u001B[41;37m  Log streaming stopped.  \u001B[0m")
     }
@@ -109,7 +109,7 @@ class ServerLogsKotlinView : Composite<FlexLayout>() {
         else -> it;
       }
     }
-    val message = "[${LocalTime.now()}][$level] Simulated log message..."
+    val message = "[${LocalTime.now()}] [$level] Simulated log message..."
     writeln(message) {
       bufferedLines--
       if (bufferedLines < LOW_WATERMARK && isStreaming && !interval.isRunning) {
@@ -119,15 +119,16 @@ class ServerLogsKotlinView : Composite<FlexLayout>() {
   }
 
   private fun Terminal.printHelp() {
+    val esc = '\u001B'
     writeln("""
-        \u001B[1;36mServer Log Stream Demo\u001B[0m
+        $esc[1;36mServer Log Stream Demo$esc[0m
 
-        \u001B[1;37mHow to use:\u001B[0m
-        - Click the \u001B[32mStart\u001B[0m button to begin streaming logs.
-        - Log levels are color-coded: \u001B[32mINFO\u001B[0m, \u001B[33mWARN\u001B[0m, \u001B[34mDEBUG\u001B[0m, \u001B[31mERROR\u001B[0m.
+        $esc[1;37mHow to use:$esc[0m
+        - Click the $esc[32mStart$esc[0m button to begin streaming logs.
+        - Log levels are color-coded: $esc[32mINFO$esc[0m, $esc[33mWARN$esc[0m, $esc[34mDEBUG$esc[0m, $esc[31mERROR$esc[0m.
         - The terminal automatically slows down if needed.
 
-        \u001B[1;33mTip:\u001B[0m Watch how buffering is managed with high-speed logs.
+        $esc[1;33mTip:$esc[0m Watch how buffering is managed with high-speed logs.
         """.trimIndent())
   }
 }
